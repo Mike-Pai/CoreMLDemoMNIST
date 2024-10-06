10fb708b62f649aa2959ac4b0b863a5fd492bf08
白謹瑜 <ye518612@gmail.com>
2024-10-07 02:06:23 +0800

    Initial Commit

---
+++ CoreMLDemoMNIT/CoreMLDemoMNITApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLDemoMNITApp.swift
+//  CoreMLDemoMNIT
+//
+//  Created by 白謹瑜 on 2024/10/7.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLDemoMNITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
